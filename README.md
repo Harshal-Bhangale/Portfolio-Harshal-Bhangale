ff5e32706da2e2aa05462015ae75d34d71bca3be
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-10-01 01:13:17 +0530

    Update

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Portfolio-Harshal-Bhangale
+# Personal Portfolio Website
 
+My name is Harshal Bhangale, and I’m a Full Stack Developer from India. My passion lies in creating software that solves real-world problems and positively impacts people’s lives.
+
+I created this website to showcase my work. I’m always striving to create something unique and innovative. I love experimenting with new technologies and staying up-to-date with the latest trends in the tech world.
+
+## Tech Stack
+
+**Client:** Java, Spring Boot, REST API, MySQL, HTML, CSS, JavaScript, AI-ML, Gen AI
+
+**Hosting:** Vercel.
+
+## Screenshots & Demo
+
+![App Screenshot](https://i.ibb.co/QCCYZyq/image.png)
+
+![App Screenshot](https://i.ibb.co/3TK00Z0/image.png)
+
+![App Screenshot](https://i.ibb.co/MGrGvsp/image.png)
+
+[View Live Demo](https://harshal-dev.vercel.app/)
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Harshal-Bhangale/harshal-dev.git
+```
+
+Go to the project directory
+
+```bash
+  cd folio-v1
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+## Credit 
+
+Harshal Bhangale
--- app/blog-section/blogDetails.ts
+++ app/blog-section/blogDetails.ts
@@ -8,28 +8,28 @@ export type blogProps = {
 
 export const blogDetails: blogProps[] = [
   // Example blog entries
-  {
-    title: "Git Init: My Web Development Journey",
-    image:
-      "https://cdn.hashnode.com/res/hashnode/image/upload/v1700476167090/03dc48bf-3f0f-4068-8836-5c83a5374f17.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-    date: "NOV 2023",
-    url: "https://mayankyadav.hashnode.dev/git-init-my-web-development-journey",
-    available: true,
-  },
-  {
-    title: "Building a Text Field Component with Material UI, Formik, and Yup",
-    image:
-      "https://cdn.hashnode.com/res/hashnode/image/upload/v1628442954056/OHNFYAzz4.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-    date: "12/31/2022",
-    url: "",
-    available: false,
-  },
-  {
-    title: "Creating a Select Field Component with Material UI, Formik, and Yup",
-    image:
-      "https://cdn.hashnode.com/res/hashnode/image/upload/v1624616590157/N4Hvd8NfZ.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-    date: "12/31/2022",
-    url: "",
-    available: false,
-  },
+  // {
+  //   title: "Git Init: My Web Development Journey",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1700476167090/03dc48bf-3f0f-4068-8836-5c83a5374f17.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+  //   date: "NOV 2023",
+  //   url: "https://mayankyadav.hashnode.dev/git-init-my-web-development-journey",
+  //   available: true,
+  // },
+  // {
+  //   title: "Building a Text Field Component with Material UI, Formik, and Yup",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1628442954056/OHNFYAzz4.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+  //   date: "12/31/2022",
+  //   url: "",
+  //   available: false,
+  // },
+  // {
+  //   title: "Creating a Select Field Component with Material UI, Formik, and Yup",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1624616590157/N4Hvd8NfZ.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+  //   date: "12/31/2022",
+  //   url: "",
+  //   available: false,
+  // },
 ];
