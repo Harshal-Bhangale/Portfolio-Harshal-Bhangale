b46d3399991c9a4f27aa693822a5ef33cb154ec0
Harshal Bhangale <99475142+Harshal-Bhangale@users.noreply.github.com>
2025-10-01 01:53:47 +0530

    Update Link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import type { Metadata, Viewport } from "next";
 import { Syne } from "next/font/google";
 
 
-export const metadata: Metadata = {
-  title: "Harshal Portfolio",
-  description: "Portfolio of Harshal Bhangale, Full Stack Developer",
-};
+// export const metadata: Metadata = {
+//   title: "",
+//   description: "Portfolio of Harshal Bhangale, Full Stack Developer",
+// };
 
 
 const syne = Syne({
@@ -15,44 +15,44 @@ const syne = Syne({
   display: "block",
   weight: ["400", "500", "600", "700", "800"],
 });
-/*
+
 export const metadata: Metadata = {
-  title: "Mayank Yadav — MERN Stack Developer",
+  title: "Harshal Bhangale - Full Stack Developer",
   description:
-    "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
+    "Developer & Innvoator || Full Stack Developer || Java Developer || Student at JSPM's Rajarshi Shahu College of Engineering, Pune.",
   generator: "Next.js",
-  applicationName: "Mayank Yadav",
-  keywords: [
-    "freelance", "developer", "frontend", "react", "creative", "tech", "ahmedabad", "software", "portfolio",
-    "mern stack developer", "backend developer", "web development", "javascript", "typescript",
-    "node.js", "express.js", "mongodb", "database", "restful API", "graphql", "programming", "coding",
-    "web design", "user interface", "user experience", "product design", "technology", "remote work",
-    "software engineering", "coding enthusiast", "digital solutions", "problem solver",
-    "innovative solutions", "agile development", "code quality",
-    "mayank yadav", "myk yadav", "yadav mayank", "mayank ldrp",
-    "mayank nptel", "yadav nptel", "myk yadav", "mayank ssip",
-    "mayank collegpt", "mayank yadav collegpt", "yadav mayank collegpt",
-    "collegpt founder", "collegpt developer", "mayank founder collegpt",
-    "mayank developer collegpt", "collegpt mayank yadav",
-    "mayank yadav safarnamaaa", "mayank yadav mongodb",
-    "yadav mayank ldrp", "yadav ldrp", "ldrpmayank", "ldrpyadav",
-    "collegpt yadav", "yadav collegpt", "mayank mern", "yadav mern",
-    "mayank developer", "mayank mern stack", "mayank yadav mern",
-    "mayank yadav react", "mayank react", "mayank hackingly"
-  ],
+  applicationName: "Harshal Bhangale",
+  // keywords: [
+  //   "freelance", "developer", "frontend", "react", "creative", "tech", "ahmedabad", "software", "portfolio",
+  //   "mern stack developer", "backend developer", "web development", "javascript", "typescript",
+  //   "node.js", "express.js", "mongodb", "database", "restful API", "graphql", "programming", "coding",
+  //   "web design", "user interface", "user experience", "product design", "technology", "remote work",
+  //   "software engineering", "coding enthusiast", "digital solutions", "problem solver",
+  //   "innovative solutions", "agile development", "code quality",
+  //   "mayank yadav", "myk yadav", "yadav mayank", "mayank ldrp",
+  //   "mayank nptel", "yadav nptel", "myk yadav", "mayank ssip",
+  //   "mayank collegpt", "mayank yadav collegpt", "yadav mayank collegpt",
+  //   "collegpt founder", "collegpt developer", "mayank founder collegpt",
+  //   "mayank developer collegpt", "collegpt mayank yadav",
+  //   "mayank yadav safarnamaaa", "mayank yadav mongodb",
+  //   "yadav mayank ldrp", "yadav ldrp", "ldrpmayank", "ldrpyadav",
+  //   "collegpt yadav", "yadav collegpt", "mayank mern", "yadav mern",
+  //   "mayank developer", "mayank mern stack", "mayank yadav mern",
+  //   "mayank yadav react", "mayank react", "mayank hackingly"
+  // ],
   // ❌ colorScheme removed from here
   openGraph: {
-    title: "Mayank Yadav — MERN Stack Developer",
+    title: "Harshal Bhangale - Full Stack Developer",
     description:
-      "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
-    url: "https://mayank-dev.vercel.app/",
-    siteName: "https://mayank-dev.vercel.app/",
+      "Developer & Innvoator || Full Stack Developer || Java Developer || Student at JSPM's Rajarshi Shahu College of Engineering, Pune.",
+    url: "https://harshal-dev.vercel.app/",
+    siteName: "https://harshal-dev.vercel.app/",
     images: [
       {
-        url: "https://i.ibb.co/jkLVTTR/image.png",
+        url: "https://i.ibb.co/NMgXZ6j/Homepage.jpg",
         width: 1200,
         height: 630,
-        alt: "Mayank Yadav — MERN Stack Developer",
+        alt: "Harshal Bhangale - Full Stack Developer",
       },
     ],
     locale: "en-US",
@@ -60,12 +60,12 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Mayank Yadav — MERN Stack Developer",
+    title: "Harshal Bhangale - Full Stack Developer",
     description:
-      "Founder & Developer at ColleGPT || MERN STACK Developer || Next Js Developer || Student at LDRP Institute of Technology & Research.",
-    creator: "mayankyadav_17",
+      "Developer & Innvoator || Full Stack Developer || Java Developer || Student at JSPM's Rajarshi Shahu College of Engineering, Pune.",
+    creator: "harshal-bhangale",
     images: [
-      "https://i.ibb.co/jkLVTTR/image.png",
+      "https://i.ibb.co/NMgXZ6j/Homepage.jpg",
     ],
   },
   robots: {
@@ -83,7 +83,7 @@ export const metadata: Metadata = {
   },
   category: "technology",
 };
-*/
+
 // ✅ Move colorScheme to a new viewport export
 export const viewport: Viewport = {
   colorScheme: "dark",
